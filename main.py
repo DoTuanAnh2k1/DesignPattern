cd5c9ca6da449c6bb98cd549fa93b346ff376469
Do Tuan Anh <anh.dt2605@gmail.com>
2024-03-23 14:32:00 +0700

    [TuanAnh] Add quick sort algorithm

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-from worker.lib import *
+from worker.lib import LibSortWorker
+from worker.quicksort import QuickSortWorker
 from common.common import *
 
 
 def Main():
-    # init worker sorting array using library
+    # Init worker sorting array using library
     LibSortWorker(
         input_file_path=PATH_INPUT,
         output_file_path=PATH_OUTPUT_LIB_SORT,
+    )
+    
+    # Init worker sorting array using quicksort algorithm
+    QuickSortWorker(
+        input_file_path=PATH_INPUT,
+        output_file_path=PATH_OUTOUT_QUICK_SORT,
     )
 
 
